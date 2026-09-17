bd1b49d724e209b8068ca11e0ebb9a154889d3fd
Willian <willianpapa132@gmail.com>
2026-09-16 21:10:24 -0300

    ajustando o test apos alteração na funcionalidade do alterar status

--- src/test/java/jpa/repository/demo/cliente/service/ClienteServiceTest.java
+++ src/test/java/jpa/repository/demo/cliente/service/ClienteServiceTest.java
@@ -5,12 +5,15 @@ import jpa.repository.demo.cliente.dto.ClienteResponseDTO;
 import jpa.repository.demo.cliente.entity.Cliente;
 import jpa.repository.demo.cliente.repository.ClienteRepository;
 import jpa.repository.demo.handler.NotFoundException;
+import jpa.repository.demo.ordemdeservico.repository.OrdemDeServicoRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -20,6 +23,9 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class ClienteServiceTest {
 
+
+    @Mock
+    OrdemDeServicoRepository ordemDeServicoRepository;
 
     @Mock
     ClienteRepository clienteRepository;
@@ -84,8 +90,9 @@ class ClienteServiceTest {
         when(clienteRepository.findById(1L)).thenReturn(Optional.of(clienteSalvo));
         when(clienteRepository.save(any(Cliente.class)))
                 .thenAnswer(invocation -> invocation.getArgument(0));
+        when(ordemDeServicoRepository.findByCliente_Id(1L)).thenReturn(null);
 
-        ClienteResponseDTO clienteResponseDTO = clienteService.mudarStatusCliente(requestDTO, 1l);
+        ClienteResponseDTO clienteResponseDTO = clienteService.mudarStatusCliente(requestDTO, 1L);
         assertNotNull(clienteResponseDTO);
         assertEquals("willian",clienteResponseDTO.getNome());
         assertFalse(clienteResponseDTO.getAtivo());
